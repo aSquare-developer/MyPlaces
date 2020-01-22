3f6d4b6d9fbf7f2af456c06a8f54e21354e7730b
Artur <artur.anissimov@gmail.com>
2020-01-22 07:02:52 +0200

    Edit Action

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -74,21 +74,32 @@ class MainViewController: UITableViewController {
     
     
         
-    /*
+
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "showDetail" {
+//            guard let indexPath = tableView.indexPathsForSelectedRows else { return }
+//            let place = places[indexPath.row]
+//            let newPlaceVC = segue.destination as! NewPlaceViewController
+//            newPlaceVC.currentPlace = place
+            if let cell = sender as? CustomTableViewCell {
+                if let indexPath = self.tableView.indexPath(for: cell) {
+                    let newPlaceVC = segue.destination as! NewPlaceViewController
+                    newPlaceVC.currentPlace = places[indexPath.row]
+                }
+            }
+        }
+        
     }
-    */
+
 
     @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
         
         guard let newPlaceVC = segue.source as? NewPlaceViewController else { return }
         
-        newPlaceVC.saveNewPlace()
+        newPlaceVC.savePlace()
         tableView.reloadData()
         
     }
